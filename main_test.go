ac093a931f66282bcbf9ddfae392718429ee213c
Bulat <babdullatypov@gmail.com>
2025-09-19 17:19:08 +0300

    Add tests for generateRandomElements

--- main_test.go
+++ main_test.go
@@ -1,3 +1,17 @@
 package main
 
-// Пишите тесты в этом файле
+
+import (
+    "testing"
+
+    //"github.com/stretchr/testify/require"
+    "github.com/stretchr/testify/assert"
+)
+
+
+func TestGenerateRandomElements(t *testing.T) {
+	for size := 0; size < 1000; size++ {
+		randSlice := generateRandomElements(size)
+		assert.Len(t, randSlice, size)
+	}
+}
