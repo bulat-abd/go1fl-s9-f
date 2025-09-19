8d9b73e88f2d04f904159249ff98cf4a4774c4ec
Bulat <babdullatypov@gmail.com>
2025-09-19 17:18:18 +0300

    Implement generateRandomElements; make project runnable

--- main.go
+++ main.go
@@ -2,38 +2,51 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 const (
 	SIZE   = 100_000_000
 	CHUNKS = 8
+	MAX    = 10_000
 )
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size <=0 {
+		return []int{}
+	}
+	result := make([]int, size)
+	for i:=0; i<size; i++ {
+		item := rand.Intn(MAX)
+		result[i] = item
+	}
+	return result
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
 	// ваш код здесь
+	return 0
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
 	// ваш код здесь
+	return 0
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
 
 	fmt.Println("Ищем максимальное значение в один поток")
 	// ваш код здесь
-
+	max := 0
+	elapsed := 0
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
 	// ваш код здесь
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
